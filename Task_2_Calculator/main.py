9eae43b37be6f9a5ef8e041ceaa483862cd9c687
20AbhishekRoka02 <uday00mishra@gmail.com>
2023-08-23 17:07:08 +0530

    compele 3 task and add gitignore
    I've completed Task 2, 3 and 5 and removed unwanted file like check.py in Task 2 and added .gitignore to ignore all .txt, .json and .png

--- Task_2_Calculator/main.py
+++ Task_2_Calculator/main.py
@@ -2,57 +2,66 @@
 from tkinter import *
 from math import sqrt
 import tkinter.messagebox as mbox
-result = ""
-contenttocalculate = []
-# userEventBuffer=""
-displaytext = ""
-displayHandlingKeys = ["C", "\u2190", "=", "."]
-numbers = [str(i) for i in range(10)]
-operators = ["+","-","*","/"]
-unaryOperators = ["!",chr(8730)]
+
+result = "" #string to store results
+contenttocalculate = [] #list that will content numbers that are being operated.
+
+displaytext = "" #text that will be displayed on screen.
+displayHandlingKeys = ["C", "\u2190", "=", "."] #Keys that handle calculator and numbers
+numbers = [str(i) for i in range(10)] #list of all number 0-9
+operators = ["+","-","*","/"] #Basic operatiors
+unaryOperators = ["!",chr(8730)] #Unary opeartors such as Factorial and Square root.
+
 class CalculatorOperation():
+
+    """This class contains methods for Calculator operations like display handling and mathematical operations """
     def __init__(self):        
         pass
 
     def btnClickEvent(self, selectedCharacter):
+        """Method controls events to be executed."""
+        # getting global variables
         global displayHandlingKeys, numbers, unaryOperators, operators, contenttocalculate, displaytext, result
             
         if selectedCharacter not in displayHandlingKeys:
             if selectedCharacter in numbers:
                 if len(result) != 0:
                     if result[-1] in operators:
+                        # for instance, if result contains "23+", so contenttocalculate list must get clear to store numbers
                         contenttocalculate.clear()
 
-                result += str(selectedCharacter)
-                contenttocalculate.append(selectedCharacter)            
+                result += str(selectedCharacter) # for instance, here, result will be "23+6", if selectedCharacter is 6 and result was "23+", otherwise it will be "236".
+                contenttocalculate.append(selectedCharacter) # and, contenttocalculate will append the number.
 
             elif selectedCharacter in unaryOperators:
                 if selectedCharacter == unaryOperators[0]: 
-                    result = self.factorial(int(eval("".join(contenttocalculate))))
-                    contenttocalculate.clear()
-                    contenttocalculate.append(result)
+                    # case of factorial
+                    result = self.factorial(int(eval("".join(contenttocalculate)))) # for instance, structure is: result = self.factorial(int(eval("".join(['2','3','+','6']))), if contenttocalculate is ['2','3','+','6']
+                    contenttocalculate.clear() # truncating the list
+                    contenttocalculate.append(result) # appending result to the list.
                 else: 
-                    result = self.squareRoot(eval("".join(contenttocalculate)))
-                    contenttocalculate.clear()
-                    contenttocalculate.append(result)
+                    # case of square root
+                    result = self.squareRoot(eval("".join(contenttocalculate))) # for instance, structure is: result = self.squareRoot(eval("".join(['2','3','+','6'])), if contenttocalculate is ['2','3','+','6']
+                    contenttocalculate.clear() # truncating the list
+                    contenttocalculate.append(result) # appending result to the list.
                 
             elif selectedCharacter in operators:
                 
                 try:
-                    result = str(eval(result))
-                    contenttocalculate.clear()
-                    contenttocalculate.append(result)
+                    result = str(eval(result)) # see, eval() can't evaluate if result is "23+". eval() is used because, if result is "23+6", then on selection of any operator, result will be "29", and can add more operators. It is also to avoid case like "23++" or "23+/"
+                    contenttocalculate.clear() # if everything went good, truncating the list
+                    contenttocalculate.append(result) # appending the value.
                 except Exception as e:
                     pass
-                result += selectedCharacter
+                result += selectedCharacter # concatinating the operator to result string.
 
-                pass
+                
         else:
             if selectedCharacter=="C":
-                self.clear()      
+                self.clear()     
             
             elif selectedCharacter=="\u2190":
-                self.backspace()
+                self.backspace() 
                 
             elif selectedCharacter == "=":
                 self.equalTo()
@@ -60,13 +69,17 @@ class CalculatorOperation():
             elif selectedCharacter == ".":
                 self.decimal()
                 
-                pass
             
-        displaytext.set("".join(contenttocalculate))
-        # displaytext.set(result)
+            
+        displaytext.set("".join(contenttocalculate)) # Setting text to be displayed on screen.
+        
 
     
     def factorial(self, num):    
+        """iterative algorithm for factorial. 
+        Here, factorial for 0 is assumed to be 1.
+        and, factorial get calculated for Integers only.
+        No number less than 0 accepted."""
         fact = 1
         if num < 0:
             mbox.showerror("Error", "Only +ve numbers accepted")
@@ -83,20 +96,24 @@ class CalculatorOperation():
         
 
     def squareRoot(self, num):
+        """Square root of number by using sqrt() method of math"""
         return str(sqrt(num))
     
     def clear(self):
+        """Resetting the result and contenttocalculate variables."""
         global result, contenttocalculate
         result=""
         contenttocalculate.clear()     
         
     def backspace(self):
+        """ remvoing Last element (being number) from result and contenttocalcualte variables."""
         global result, operators, contenttocalculate
         if result[-1] not in operators:
             result = result[:-1]
             contenttocalculate.pop()
 
     def equalTo(self):
+        """Return result of equal to operation."""
         global result, contenttocalculate
         try:
             result = eval(result)
@@ -106,21 +123,26 @@ class CalculatorOperation():
             pass
 
     def decimal(self):
+        """Method to handle, when user selects '.' (dcimal) in calculator."""
         global result, contenttocalculate
 
         if result[-1] in operators:
+            # if result is "23+", then on this case, on clicking '.', result will be "23+0."
             result += "0."
             contenttocalculate.clear()
             contenttocalculate.append("0.")
         elif len(result)==0:
+            # if there is nothing in result andb contenttocalculate, simply add "0."
             result += "0."
             contenttocalculate.append("0.")
         else:
+            # otherwise, there will be numbers, so, simply add '.' such as "23."
             result += "."
             contenttocalculate.append(".")
-        pass
+        
 
 class Calculator():
+    """Calculator GUI class to handle GUI working."""
     def __init__(self):                 
             
         root = Tk()
@@ -128,49 +150,44 @@ class Calculator():
         # root window title and dimension
         root.title("Calculator GUI")
         # Set geometry (widthxheight)
-        root.geometry('340x560')
-        # root.resizable(width=False, height=False)
+        root.geometry('340x560')        
         root.configure(bg="#eee9e9")
 
-        # displaycontent = []
+        # getting global variable displaytext.
         global displaytext
-        displaytext = StringVar()
-        display = Entry(root, width=300, textvariable=displaytext,font=("Segoe UI bold", 50),  cursor="arrow")
-        # display.grid(row=0, column=0)
+        displaytext = StringVar() # creating object of class StringVar.
+        display = Entry(root, width=300, textvariable=displaytext,font=("Segoe UI bold", 50),  cursor="arrow")        
         display.pack()
 
-        # Buttons
+        # Buttons -> list of symbols that will be displayed on Calculator.
         symbolSet = ["C",chr(8730), "/","\u2190", 7,8,9, "*", 4,5,6,"-",1,2,3,"+","!",0,".","="]
 
+        # Creating frame to add and display buttons 
         btnFrame = Frame(root)
         btnFrame.pack()
 
-
+        # creating object of class CalculatorOperations
         calculate = CalculatorOperation()
 
-
-        def btnSet(symbol, index):                        
+        # nested function to add buttons on screen.
+        def btnSet(symbol, index):
+            # buttons to be dispalyed                        
             btn= Button(btnFrame,text=str(symbol), font=("Segoe UI", 23), padx=20, pady=10, command=lambda t=symbol: calculate.btnClickEvent(str(t)))
-            btn.grid(row=index//4, column=index%4, sticky="nsew")
+            btn.grid(row=index//4, column=index%4, sticky="nsew") # Giving position to buttons in frame.
             
 
-        # map(btnSet, numbers)
+        # adding buttons in frame
         for symbol in symbolSet:
             btnSet(symbol, symbolSet.index(symbol))
-        # for i in range(10):
-        #     btn = Button(root, text=str(i))
-        #     btn.grid(row=(i%4)+1, column=i%4)
         
-
-
         root.mainloop()
 
-        pass
+    
     
 
         
         
-        pass
+    
 
 if __name__=="__main__":
     app = Calculator()
--- Task_3_RandomPasswordGenerator/main.py
+++ Task_3_RandomPasswordGenerator/main.py
@@ -23,9 +23,6 @@ class PasswordGenerator:
     def generatePassword(self,length=8):
         """
         The generatePassword() method by default has 8 characters long password.
-
-        Length less than 4 is not acceptable, by the way this is not a good practice to have password 4 characters long password.
-
         PASSWORD MUST BE ATLEAST 8 CHARACTERS LONG.
         """
         if (length < 8):
@@ -59,62 +56,54 @@ class PasswordGenerator:
     
 
 class MainApp:
-    
+    """Class to handle UI of GUI"""
 
     def __init__(self):
         def reset(event):
+            """On reset, all text from the input fields get removed."""
             username.delete("1.0","end")
             passLength.delete("1.0","end")
             generatedPass.delete("1.0","end")
         
         def accept(event):
+            """To handled on Clicking Accept operation."""
             if len(generatedPass.get("1.0","end"))==1:
+                # if password field empty
                 mbox.showerror("Error", "Nothing to copy on clipboard!")
             else:
-                # print(generatedPass.get("1.0","end"),len(generatedPass.get("1.0","end")),end="\n")
-                root.clipboard_clear()
-                root.clipboard_append(generatedPass.get("1.0","end-1c"))
-                # root.update()
-                mbox.showinfo("Information","Password is copied to clipboard!")
+                
+                root.clipboard_clear() # removing everything from clipboard.
+                root.clipboard_append(generatedPass.get("1.0","end-1c")) # appending password to clipboard.                
+                mbox.showinfo("Information","Password is copied to clipboard!") # displaying message to user.
 
 
         root = Tk()
-
         # root window title and dimension
         root.title("Random Password Generator")
         # Set geometry (widthxheight)
         root.geometry('500x400')
-        root.resizable(width=False, height=False)
-
-        # root = Frame()
-        # self.main_frame = Frame()
-
-        # root.columnconfigure(0,weight=1)
-        # root.rowconfigure(0,weight=1)
-        # root.rowconfigure(2,weight=1)
-        # root.rowconfigure(3,weight=1)
+        root.resizable(width=False, height=False) # Resize disabled.
 
 
-        
-
-        # heading=ttk.Label(root, text="Hello World!")
-        # heading.grid(column=0, row=0)
-
+        # Adding title to screen
         titleLable = Label(root, text="Password Generator", anchor="center", fg="#003153", font='Helvetica 18 bold underline')
 
+        # user label and field
         usernameLabel = Label(root, text="Enter user name: ", anchor="w")
         username=Text(root,bg="#ffffff",height=1, width=20)
 
+        # password label and field
         passLengthLabel = Label(root, text="Enter password length: ", anchor="w")
-
-        # global passLength
         passLength=Text(root, height=1, width=20)
 
+        # Generated password label and field
         generatedPassLabel = Label(root, text="Generated   password: ", anchor="w")
         generatedPass = Text(root, height=1, width=20, fg="#07a607")
 
+        # Generate password button
         generatePassbtn = Button(root, text="GENERATE PASSWORD", bg="#0b0bba", fg="#ffffff", command=lambda: self.passwordGeneration(passLength.get("1.0","end-1c"), generatedPass, username))
 
+        # Accept button 
         canvas_accept = Canvas(root, width=100, height=40, highlightthickness=0)
         accept_rectangle = canvas_accept.create_rectangle(20, 10, 80, 30, outline="black", fill="white")
         accept_text = canvas_accept.create_text(50, 20, text="ACCEPT")
@@ -122,11 +111,7 @@ class MainApp:
         canvas_accept.tag_bind(accept_text,"text-accept")
         canvas_accept.bind("<Button-1>",accept)
 
-
-
-        # accept = Button(root, text="ACCEPT", fg="#0b0bba", bg="#ffffff")
-        # reset = Button(root, text="RESET", fg="#0b0bba",  bg="#ffffff")
-
+        # Reset Button       
         canvas_reset = Canvas(root, width=100, height=40, highlightthickness=0)
         reset_rectangle = canvas_reset.create_rectangle(20, 10, 80, 30, outline="black", fill="white")
         reset_text = canvas_reset.create_text(50, 20, text="RESET")
@@ -139,7 +124,7 @@ class MainApp:
 
         
         
-        
+        # Placing things on screen.
         titleLable.grid(column=1, row=0)
 
         usernameLabel.grid(column=0, row=1)
@@ -152,54 +137,35 @@ class MainApp:
         generatedPass.grid(column=1, row=3 )
 
         generatePassbtn.grid(column=1, row=4)
-        # accept.grid(column=1, row=5)
-        # reset.grid(column=1, row=6)
+        
         canvas_accept.grid(column=1, row=5)
         canvas_reset.grid(column=1, row=6)
 
-        # root.place(in_=self.main_frame, anchor="c", relx=.50, rely=.50)
-
-
-        
-
-        # all widgets will be here
-        # Execute Tkinter
         root.mainloop()
 
     
 
         
     def passwordGeneration(self, length, geneartedPassField, usernameField):
+        """Field to check input fields and calling function to generate password"""
+        if len(usernameField.get("1.0","end-1c"))!=0:
+            try: 
+                length = int(length)
+
+                password = PasswordGenerator()
+                geneartedPassField.delete("1.0","end")
+                geneartedPassField.insert("1.0",password.generatePassword(length))
+
+            except Exception as e:
+                if length < 4:
+                    mbox.showwarning("Warning",length)
+                else:
+                    mbox.showwarning("Warning", "Enter password length as positive number!" )
+
+        else:
+            mbox.showwarning("Warning", "Please enter username!")
+
         
-            if len(usernameField.get("1.0","end-1c"))!=0:
-                try: 
-                    length = int(length)
-
-                    password = PasswordGenerator()
-                    geneartedPassField.delete("1.0","end")
-                    geneartedPassField.insert("1.0",password.generatePassword(length))
-
-                except Exception as e:
-                    if length < 4:
-                        mbox.showwarning("Warning",length)
-                    else:
-                        mbox.showwarning("Warning", "Enter password length as positive number!" )
-
-            else:
-                mbox.showwarning("Warning", "Please enter username!")
-
-
-        
-
-    def acceptPassword(self):
-        pass
-
-    def resetPassword(self, event, username, passlength, generatedPass):
-        """How to work with event on Canvas"""
-        username.delete("1.0","end")
-        passlength.delete("1.0","end")
-        generatedPass.delete("1.0","end")
-        pass
 
 if __name__ == "__main__":
     app = MainApp()
